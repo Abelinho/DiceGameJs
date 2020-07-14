8b0d57b48381ea41d943913f643ce2ef8094641c
Abelinho <40649905+Abelinho@users.noreply.github.com>
2020-07-14 12:48:15 +0100

    Add initial Dice Game Files

---
+++ index.js
@@ -0,0 +1,56 @@
+var randomNumber1 =  Math.floor(Math.random() * 6) + 1;
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+switch (randomNumber1) {
+    case 1:
+      document.querySelector(".img1").setAttribute("src","images/dice1.png");
+      break;
+    case 2:
+       document.querySelector(".img1").setAttribute("src","images/dice2.png");
+      break;
+    case 3:
+      document.querySelector(".img1").setAttribute("src","images/dice3.png");
+      break;
+    case 4:
+      document.querySelector(".img1").setAttribute("src","images/dice4.png");
+      break;
+    case 5:
+      document.querySelector(".img1").setAttribute("src","images/dice5.png");
+      break;
+    case 6:
+      document.querySelector(".img1").setAttribute("src","images/dice6.png");
+  }
+
+
+  switch (randomNumber2) {
+    case 1:
+      document.querySelector(".img2").setAttribute("src","images/dice1.png");
+      break;
+    case 2:
+       document.querySelector(".img2").setAttribute("src","images/dice2.png");
+      break;
+    case 3:
+      document.querySelector(".img2").setAttribute("src","images/dice3.png");
+      break;
+    case 4:
+      document.querySelector(".img2").setAttribute("src","images/dice4.png");
+      break;
+    case 5:
+      document.querySelector(".img2").setAttribute("src","images/dice5.png");
+      break;
+    case 6:
+      document.querySelector(".img2").setAttribute("src","images/dice6.png");
+  }
+
+  if(randomNumber1 > randomNumber2){
+      document.querySelector("h1").textContent = "Player 1 wins!";
+  }
+
+  else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").textContent = "Player 2 wins!";
+  }
+
+  else{
+    document.querySelector("h1").textContent = "Its a draw!!";
+  }
